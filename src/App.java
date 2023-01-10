2835877a9806a50a5cc2d1c18a52ed797c9bc89d
Rafael <rafinha_fern@hotmail.com>
2023-01-10 14:33:22 -0300

    feat: new pyramid of numbers 1 to 9

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        for (int i = 1; i < 10; i++) {
+            String text = "";
+            for (int y = 1; y <= i; y++) {
+                text = text + i;
+            }
+            System.out.println(text);
+        }
+
     }
 }
